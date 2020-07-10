d78278664b0161aa37409bcc8a89ed225db485e6
Annie Liao <liaowow@gmail.com>
2020-07-10 19:55:17 -0400

    added AJAX functionality

--- src/scripts/sections/featured-collection.js
+++ src/scripts/sections/featured-collection.js
@@ -15,11 +15,51 @@ import {register} from '@shopify/theme-sections';
  * @param {string} container - selector for the section container DOM element
  */
 register('featured-collection', {
-  onLoad() {
+  init() {
     this.publicMethod();
   },
 
   publicMethod() {
     window.console.log('Initialising featured collection section');
+  },
+
+  onLoad() {
+
+    const cartBtns = document.querySelectorAll('.product-card__add-to-cart');
+    cartBtns.forEach((btn) => btn.addEventListener('click', (event) => {
+      event.preventDefault();
+
+      const config = {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          'quantity': 1,
+          'id': btn.dataset.variantId
+        })
+      };
+  
+      window.fetch('./cart/add.js', config)
+        .then((response) => {
+          if (response.status === 200) {
+            updateCount();
+          }
+          return true;
+        })
+        .catch((err) => window.console.error(err));
+  
+    }));
+
+    function updateCount() {
+      const cartItemCount = document.querySelector('.site-header__item-count')
+
+      window.fetch('./cart.js')
+              .then(r => r.json())
+              .then(data => cartItemCount.innerHTML = `(${data.item_count})`)
+              .catch(err => console.error(err))
+      
+    }
   },
 });
