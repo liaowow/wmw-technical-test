5e228ae924ad2b1463ede182cf8e21af8ea29b84
Annie Liao <liaowow@gmail.com>
2020-07-10 17:50:17 -0400

    changed init() to onLoad() in register

--- src/scripts/sections/featured-collection.js
+++ src/scripts/sections/featured-collection.js
@@ -15,32 +15,11 @@ import {register} from '@shopify/theme-sections';
  * @param {string} container - selector for the section container DOM element
  */
 register('featured-collection', {
-  init() {
+  onLoad() {
     this.publicMethod();
   },
 
   publicMethod() {
     window.console.log('Initialising featured collection section');
   },
-
-  // cartItemClicked() {
-  //   const currentItem = document.querySelector('.product-card__add-to-cart');
-  //   currentItem.addEventListener('click', () => window.console.log('clicked!'));
-  // },
-
-  // addToCart(id) {
-
-    // const config = {
-    //   method: 'POST',
-    //   headers: {
-    //     'content-type': 'application/json',
-    //   },
-    //   body: JSON.stringify(id),
-    // };
-
-    // fetch('/cart', config)
-    //   .then(res => res.json())
-    //   .then(data => window.console.log(data))
-    //   .catch(err => window.console.log(`Oops, here's the error: ${err}`));
-  // },
 });
